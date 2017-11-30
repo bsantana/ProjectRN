bad853f91e4a80486bec41c4be97a39c103e5192
bsantana <bruno_shenn@hotmail.com>
2017-11-30 02:05:34 -0200

    Salvando projeto 1

--- app1/index.android.js
+++ app1/index.android.js
@@ -1,53 +1,32 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
   Text,
-  View
+  View,
+  Button,
 } from 'react-native';
+
+const gerarNumeroAleatorio = () => {
+  let numero_aleatorio = Math.random();
+
+  numero_aleatorio *= 10;
+  //numero_aleatorio = Math.floor(numero_aleatorio)
+  numero_aleatorio = parseInt(numero_aleatorio);
+  alert(numero_aleatorio);
+};
 
 export default class app1 extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View>
+        <Text>Gerador de números randômicos!!!</Text>
+        <Button 
+          title='Gerar um número randômico'
+          onPress={gerarNumeroAleatorio}
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('app1', () => app1);
